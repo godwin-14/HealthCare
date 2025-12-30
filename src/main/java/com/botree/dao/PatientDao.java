1f5842fb1097a373993724b2bcbd22d2de8d3267
godwin-14 <godwin.kanagaraj@botree.co.in>
2025-12-30 14:44:28 +0530

    added more dao

--- src/main/java/com/botree/dao/PatientDao.java
+++ src/main/java/com/botree/dao/PatientDao.java
@@ -1,5 +1,12 @@
 package com.botree.dao;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class PatientDao {
 
+	@Autowired
+	private SessionFactory sessionFactory;
 }
