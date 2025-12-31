9d54a66183e21799df144901b7f60795783be86e
geetha <geetha.narayanan@pg02svnk.bsilind.local>
2025-12-31 10:40:31 +0530

    Healthcard Commit

---
+++ src/main/java/com/botree/bean/HealthCardBean.java
@@ -0,0 +1,58 @@
+package com.botree.bean;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.botree.dao.PatientDao;
+import com.botree.model.Patient;
+
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+
+@Component
+@Scope("session")
+public class HealthCardBean {
+
+	@Autowired
+	private PatientDao patientDao;
+
+	private Integer patientId;
+	private Patient patient;
+
+	public Integer getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(Integer patientId) {
+		this.patientId = patientId;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public void findPatient() {
+
+		if (patientId == null) {
+			FacesContext.getCurrentInstance().addMessage("msgs",
+					new FacesMessage(FacesMessage.SEVERITY_WARN, "Input Required", "Please enter Patient ID"));
+			return;
+		}
+
+		var list = patientDao.searchById(patientId);
+
+		if (!list.isEmpty()) {
+			patient = list.get(0);
+		} else {
+			patient = null;
+			FacesContext.getCurrentInstance().addMessage("msgs",
+					new FacesMessage(FacesMessage.SEVERITY_WARN, "Not Found", "No patient found with ID " + patientId));
+		}
+	}
+
+}
--- src/main/java/com/botree/dao/PatientDao.java
+++ src/main/java/com/botree/dao/PatientDao.java
@@ -1,12 +1,29 @@
 package com.botree.dao;
 
+import java.util.List;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.botree.model.Patient;
 
 @Repository
 public class PatientDao {
 
 	@Autowired
 	private SessionFactory sessionFactory;
+	
+	
+	public List<Patient> searchById(int pid)
+	{
+	    var session = sessionFactory.openSession();
+	    var trans =  session.beginTransaction();
+	    var query = session.createQuery("from Patient p where p.pid = :pid", Patient.class);
+	    query.setParameter("pid", pid);
+	    var list = query.getResultList();
+	    session.close();
+	    trans.commit();
+	    return list;
+	}
 }
