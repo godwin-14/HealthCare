03be9fc6352ab26ce962681943ecf0d098edbf03
sirisha <sirishanarasimha@botree.co.in>
2025-12-31 10:40:34 +0530

    Merge branch 'master' of https://github.com/godwin-14/HealthCare.git into sirishadev
    Signed-off-by: sirisha <sirishanarasimha@botree.co.in>

--- src/main/java/com/botree/dao/PatientDao.java
+++ src/main/java/com/botree/dao/PatientDao.java
@@ -1,12 +1,22 @@
 package com.botree.dao;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.botree.model.Patient;
 
 @Repository
 public class PatientDao {
 
-	@Autowired
-	private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Transactional
+    public void update(Patient patient) {
+        sessionFactory.getCurrentSession().update(patient);
+    }
+
 }
